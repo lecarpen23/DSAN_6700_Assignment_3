89b721381f14b39f864c4d4d54badd56d5343d0f
Landon Carpenter <77710525+lecarpen23@users.noreply.github.com>
2023-10-14 14:59:55 -0400

    Add files via upload
    initial

---
+++ Assignment 3 Supporting Code.py
@@ -0,0 +1,383 @@
+﻿import random
+from seaborn.palettes import color_palette
+random.seed(9001)
+from sklearn.datasets import load_breast_cancer
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+Bike = pd.read_csv("datasets/Bike-Sharing-Hour.csv",header=0)
+
+
+Bike.rename(columns={'instant':'rec_id',
+                        'dteday':'datetime',
+                        'holiday':'is_holiday',
+                        'workingday':'is_workingday',
+                        'weathersit':'weather_condition',
+                        'hum':'humidity',
+                        'mnth':'month',
+                        'cnt':'total_count',
+                        'hr':'hour',
+                        'yr':'year'}, inplace=True)
+
+###########################
+# Setting proper data types
+###########################
+# date time conversion
+Bike['datetime'] = pd.to_datetime(Bike.datetime)
+
+# categorical variables
+Bike['season'] = Bike.season.astype('category')
+Bike['is_holiday'] = Bike.is_holiday.astype('category')
+Bike['weekday'] = Bike.weekday.astype('category')
+Bike['weather_condition'] = Bike.weather_condition.astype('category')
+Bike['is_workingday'] = Bike.is_workingday.astype('category')
+Bike['month'] = Bike.month.astype('category')
+Bike['year'] = Bike.year.astype('category')
+Bike['hour'] = Bike.hour.astype('category')
+
+Bike.head()
+
+
+corrMatt = Bike[['temp',
+                    'atemp', 
+                    'humidity', 
+                    'windspeed', 
+                    'casual', 
+                    'registered', 
+                    'total_count']].corr()
+
+mask = np.array(corrMatt)
+# Turning the lower-triangle of the array to false
+mask[np.tril_indices_from(mask)] = False
+fig,ax = plt.subplots()
+sns.heatmap(corrMatt, 
+            mask=mask,
+            vmax=.8, 
+            square=True,
+            annot=True,
+)
+
+
+# Defining categorical variables encoder method
+def fit_transform_ohe(df,col_name):
+    """This function performs one hot encoding for the specified
+column.
+    Args:
+        df(pandas.DataFrame): the data frame containing the mentioned column name
+        col_name: the column to be one hot encoded
+    Returns:
+        tuple: label_encoder, one_hot_encoder, transformed column as pandas Series
+    """
+    # label encode the column
+    le = preprocessing.LabelEncoder()
+    le_labels = le.fit_transform(df[col_name])
+    df[col_name+'_label'] = le_labels
+    # one hot encoding
+    ohe = preprocessing.OneHotEncoder()
+    feature_arr = ohe.fit_transform(df[[col_name+'_label']]).toarray()
+    feature_labels = [col_name+'_'+str(cls_label) for cls_label in le.classes_]
+    features_df = pd.DataFrame(feature_arr, columns=feature_labels)
+    return le,ohe,features_df
+
+# given label encoder and one hot encoder objects, 
+# encode attribute to ohe
+def transform_ohe(df,le,ohe,col_name):
+    """This function performs one hot encoding for the specified
+        column using the specified encoder objects.
+
+    Args:
+        df(pandas.DataFrame): the data frame containing the mentioned column name
+        le(Label Encoder): the label encoder object used to fit label encoding
+        ohe(One Hot Encoder): the onen hot encoder object used to fit one hot encoding
+        col_name: the column to be one hot encoded
+
+    Returns:
+        tuple: transformed column as pandas Series
+
+    """
+    # label encode
+    col_labels = le.transform(df[col_name])
+    df[col_name+'_label'] = col_labels
+    
+    # ohe 
+    feature_arr = ohe.fit_transform(df[[col_name+'_label']]).toarray()
+    feature_labels = [col_name+'_'+str(cls_label) for cls_label in le.classes_]
+    features_df = pd.DataFrame(feature_arr, columns=feature_labels)
+    
+    return features_df
+# Divide the dataset into training and testing sets
+X, X_test, y, y_test = train_test_split(Bike.iloc[:,0:-3],
+                                        Bike.iloc[:,-1],
+                                        test_size=0.33,
+                                        random_state=42)
+X.reset_index(inplace=True)
+y = y.reset_index()
+
+X_test.reset_index(inplace=True)
+y_test = y_test.reset_index()
+# Encoding all the categorical features
+cat_attr_list = ['season','is_holiday',
+                 'weather_condition','is_workingday',
+                 'hour','weekday','month','year']
+# though we have transformed all categoricals into their one-hot encodings, note that ordinal
+# attributes such as hour, weekday, and so on do not require such encoding.
+numeric_feature_cols = ['temp','humidity','windspeed',
+                        'hour','weekday','month','year']
+subset_cat_features =  ['season','is_holiday','weather_condition','is_workingday']
+
+###############
+# Train dataset
+###############
+encoded_attr_list = []
+for col in cat_attr_list:
+    return_obj = fit_transform_ohe(X,col)
+    encoded_attr_list.append({'label_enc':return_obj[0],
+                              'ohe_enc':return_obj[1],
+                              'feature_df':return_obj[2],
+                              'col_name':col})
+
+
+feature_df_list  = [X[numeric_feature_cols]]
+feature_df_list.extend([enc['feature_df'] \
+                        for enc in encoded_attr_list \
+                        if enc['col_name'] in subset_cat_features])
+
+train_df_new = pd.concat(feature_df_list, axis=1)
+print("Train dataset shape::{}".format(train_df_new.shape))
+print(train_df_new.head())
+
+##############
+# Test dataset
+##############
+test_encoded_attr_list = []
+for enc in encoded_attr_list:
+    col_name = enc['col_name']
+    le = enc['label_enc']
+    ohe = enc['ohe_enc']
+    test_encoded_attr_list.append({'feature_df':transform_ohe(X_test,
+                                                              le,ohe,
+                                                              col_name),
+                                   'col_name':col_name})
+    
+    
+test_feature_df_list = [X_test[numeric_feature_cols]]
+test_feature_df_list.extend([enc['feature_df'] \
+                             for enc in test_encoded_attr_list \
+                             if enc['col_name'] in subset_cat_features])
+
+test_df_new = pd.concat(test_feature_df_list, axis=1) 
+print("Test dataset shape::{}".format(test_df_new.shape))
+print(test_df_new.head())
+
+#%%
+from matplotlib import pyplot as plt
+import seaborn as sns
+bike_data = Bike[['temp',
+                    'atemp', 
+                    'humidity', 
+                    'windspeed', 
+                    'casual', 
+                    'registered', 
+                    'total_count']]
+
+bike_data.head()
+
+rc = {'figure.figsize':(10,10),
+      'axes.facecolor':'white',
+       'axes.grid' : True,
+       'grid.color': '.9',
+       'grid.linestyle':'-.',
+       'font.family':'Times New Roman',
+       'font.size' : 15}
+plt.rcParams.update(rc)
+sns.pairplot(bike_data,hue="total_count",palette="vlag")
+
+import plotly.express as px
+
+fig = px.scatter_matrix(bike_data, color='total_count')
+fig.update_layout(
+    title='Bike Sharing Dataset',
+    dragmode='select',
+    width=600,
+    height=600,
+    hovermode='closest',
+)
+fig.show()
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_boston
+import warnings
+from sklearn.model_selection import cross_val_score, RepeatedKFold
+from sklearn.linear_model import Ridge, Lasso, LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor, BaggingRegressor, RandomForestRegressor, StackingRegressor
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor, sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+# Bike = Bike.drop(['datetime'], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(Bike.iloc[:,0:-3],
+                                        Bike.iloc[:,-1],
+                                        test_size=0.33,
+                                        random_state=42)
+# one-hot encode the categorical features
+cat_attribs = Bike[['season','is_holiday','weekday','weather_condition','is_workingday','month','year','hour']]
+full_pipeline = ColumnTransformer([('cat', OneHotEncoder(handle_unknown='ignore'), cat_attribs)], remainder='passthrough')
+
+
+
+def base_models():
+  models = dict()
+  models['lr'] = LinearRegression()
+  models["Ridge"] = Ridge()
+  models["Lasso"] = Lasso()
+  models["Tree"] = DecisionTreeRegressor()
+  models["Random Forest"] = RandomForestRegressor()
+  models["Bagging"] = BaggingRegressor()
+  models["GBM"] = GradientBoostingRegressor()
+  models["LightGBM"] = LGBMRegressor()
+  return models
+
+  # Function to evaluate the list of models
+def eval_models(model):
+  cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+  scores = -cross_val_score(model, X_train, y_train, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1, 
+                            error_score='raise')
+  return scores
+
+models = base_models()
+# evaluate the models and store results
+results, names = list(), list() 
+
+for name, model in models.items():
+  scores = eval_models(model)
+  results.append(scores)
+  names.append(name)
+  print('>%s %.3f (%.3f)' % (name, scores.mean(), scores.std()))
+
+
+regressmod = pd.DataFrame(np.transpose(results), columns = ["lr","Ridge","Lasso","Tree","Random Forest","Bagging","GBM", "LightGBM"])
+regressmod = pd.melt(regressmod.reset_index(), id_vars='index',value_vars=["lr","Ridge","Lasso","Tree","Random Forest","Bagging","GBM", "LightGBM"])
+
+
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+
+fig = px.box(regressmod, x="variable", y="value",color="variable",points='all',
+labels={"variable": "Machine Learning Model",
+        "value": "RMS Error"
+        },title="Model Performance")
+fig.show()
+
+
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_boston
+import warnings
+from sklearn.model_selection import cross_val_score, RepeatedKFold
+from sklearn.linear_model import Ridge, Lasso, LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor, BaggingRegressor, RandomForestRegressor, StackingRegressor
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+
+def get_stacking():
+	# define the base models
+  level0 = list()
+  level0.append(('Tree', DecisionTreeRegressor()))
+  level0.append(('RF', RandomForestRegressor()))
+  level0.append(('Bagging', BaggingRegressor()))
+  level0.append(('GBM', GradientBoostingRegressor()))
+	# define meta learner model
+  level1 = LGBMRegressor()
+	# define the stacking ensemble
+  model = StackingRegressor(estimators=level0, final_estimator=level1, cv=5)
+  return model
+
+def base_models():
+  models = dict()
+  models["Tree"] = DecisionTreeRegressor()
+  models["Random Forest"] = RandomForestRegressor()
+  models["Bagging"] = BaggingRegressor()
+#   models["XGB"] = XGBRegressor()
+  models["Stacked Model"] = get_stacking()
+  return models
+
+# Function to evaluate the list of models
+def eval_models(model):
+  cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+  scores = -cross_val_score(model, X_train, y_train, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1, 
+                            error_score='raise')
+  return scores
+
+models = base_models()
+# evaluate the models and store results
+results, names = list(), list() 
+
+for name, model in models.items():
+  scores = eval_models(model)
+  results.append(scores)
+  names.append(name)
+  print('>%s %.3f (%.3f)' % (name, scores.mean(), scores.std()))
+
+
+
+regressmod1 = pd.DataFrame(np.transpose(results), columns = ["Tree","Random Forest","Bagging","Stacked Reg"])
+regressmod1 = pd.melt(regressmod1.reset_index(), id_vars='index',value_vars=["Tree","Random Forest","Bagging","Stacked Reg"])
+fig = px.box(regressmod1, x="variable", y="value",color="variable",points='all',
+labels={"variable": "Machine Learning Model",
+        "value": "RMS Error"
+        },title="Model Performance")
+fig.show()
+# fig.write_image("HW3/Boxplot-candidate.jpeg",engine="kaleido",format="png", width=1600, height=700, scale=0.75)
+fig.show()
+
+
+import pickle
+
+level0 = list()
+level0.append(('Tree', DecisionTreeRegressor()))
+level0.append(('RF', RandomForestRegressor()))
+level0.append(('GBM', GradientBoostingRegressor()))
+level0.append(('Bagging', BaggingRegressor()))
+
+level1 = LGBMRegressor()
+model = StackingRegressor(estimators=level0, final_estimator=level1, cv=5)
+model.fit(X_train, y_train)
+
+# Save to file in the current working directory
+pkl_filename = "AssignmentPickle.pkl"
+with open(pkl_filename, 'wb') as file:
+    pickle.dump(model, file)
+
+# Load from file
+with open(pkl_filename, 'rb') as file:
+    pickle_model = pickle.load(file)
+
+
+score = pickle_model.score(X_test, y_test)
+print("Test score: {0:.2f} %".format(100 * score))
+Y_predict = pickle_model.predict(X_test)
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+predictions = pd.DataFrame(Y_predict, columns=['predictions'])
+predictions['actual'] = y_test
+plt.scatter(x = y_test, y = Y_predict, color='#336699',alpha=0.6)
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.title('Predicted vs Actual', fontsize=15, color='#336699',loc='center')
+
